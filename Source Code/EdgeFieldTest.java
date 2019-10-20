3298c535ef4811bfdcdd759e311e864d9a21f749
Joungho Seo(Jo) <54644247+joungho21@users.noreply.github.com>
2019-10-19 23:06:22 -0400

    Update EdgeFieldTest.java

--- Source Code/EdgeFieldTest.java
+++ Source Code/EdgeFieldTest.java
@@ -34,56 +34,56 @@ public class EdgeFieldTest {
 	public void testGetNumFigure() {
 		assertEquals("NumFigure was set to 123",123,testObj.getNumFigure());
 	}
-
+	@Test
 	public void testGetName() {
 		assertEquals("Name was set to test","test",testObj.getName());
 	}
-
+	@Test
 	public void testTableID() {
 		testObj.setTableID(5);
 		assertEquals("TableID set to 5",5,testObj.getTableID());
 	}
-
+	@Test
 	public void testTableBound() {
 		testObj.setTableBound(8);
 		assertEquals("TableBound set to 8",8,testObj.getTableBound());
 	}
-
+	@Test
 	public void testFieldBound() {
 	        testObj.setFieldBound(1);
 	 	assertEquals("FieldBound set to 1",1,testObj.getFieldBound());
 	}
-
+	@Test
 	public void testDisallowNull() {
 		testObj.setDisallowNull(false);
 		assertEquals("DisallowNull set to false",false,testObj.getDisallowNull());
 	}
-
+	@Test
 	public void testIsPrimaryKey() {
 		testObj.setIsPrimaryKey(true);
 		assertEquals("IsPrimaryKey set to true",true,testObj.getIsPrimaryKey());
 	}
-
+	@Test
 	public void testDefaultValue() {
 		testObj.setDefaultValue("testV2");
 		assertEquals("Default Value set to testV2","testV2",testObj.getDefaultValue());
 	}
-
+	@Test
 	public void testVarcharValue() {
 		testObj.setVarcharValue(314);
 		assertEquals("VarcharValue set to 314",314,testObj.getVarcharValue());
 	}
-
+	@Test
 	public void testDataType() {
 		testObj.setDataType(1);
 		assertEquals("DataType set to 1 (Boolean)",1,testObj.getDataType());
 	}
-
+	@Test
 	public void testGetStrDataType() {
 		String[] testArry = {"Varchar", "Boolean", "Integer", "Double"};
 		assertEquals("GetStrDataType should return what it was initailized in the beginning= {varchar,boolean,int,double}",testArry,testObj.getStrDataType());
 	}
-
+	@Test
 	public void testToString() {
 		String result = "123|test|5|8|1|1|314|true|false|testV2";
 		assertEquals("To String should return all the values set duing the test",result,testObj.toString());
