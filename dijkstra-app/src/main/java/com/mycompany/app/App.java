353a9f43b2d547f63b8717c63d9c618ef64c8aac
Joungho Seo(Jo) <54644247+joungho21@users.noreply.github.com>
2019-11-12 20:56:28 -0500

    Add files via upload

---
+++ dijkstra-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * 
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        EdgeConvertGUI edge = new EdgeConvertGUI();
+    }
+}
